c083e6c39ff7726fca75eb798f26b71c2a9d5671
wumiao <wumiao@xiimoon.com>
2020-08-24 19:06:32 +0800

    Skill Test

---
+++ Unity/Assets/SkillSystem/Script/Common/CharacterSkillManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSkillManager : MonoBehaviour
+{
+    public SkillData[] skilldatas;
+    
+    
+}
