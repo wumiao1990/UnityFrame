ccb67d0e687f8b3bab4cd359e3427d618cafec7a
wumiao <wumiao@xiimoon.com>
2020-09-12 10:47:47 +0800

    UIControlBind

--- Unity/Assets/SkillSystem/Script/Common/CharacterSkillManager.cs
+++ Unity/Assets/SkillSystem/Script/Common/CharacterSkillManager.cs
@@ -6,5 +6,21 @@ public class CharacterSkillManager : MonoBehaviour
 {
     public SkillData[] skilldatas;
     
+    private void Start()
+    {
+        for(int i = 0; i<skilldatas.Length;i++)
+        {
+            InitSkill(skilldatas[i]);
+        }
+    }
+    public void InitSkill(SkillData data)
+    {
+        
+    }
+    //生成技能
+    public void GenterateSkill()
+    {
+
+    }
     
 }
---
+++ Unity/Assets/UIControlBind/Scripts/UIManagerControl.cs
@@ -0,0 +1,28 @@
+﻿using SDGame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SDGame.UITools;
+
+public class UIManagerControl : UnitySingleton<UIManagerControl>
+{
+
+	void Start () {
+        // TODO get config from xml
+        IBindableUI uiA = Activator.CreateInstance(Type.GetType("UIA")) as IBindableUI;
+        GameObject prefab = Resources.Load<GameObject>("UI/UIA");
+        GameObject go = Instantiate(prefab);
+        UIControlData ctrlData = go.GetComponent<UIControlData>();
+        if(ctrlData != null)
+        {
+            ctrlData.BindDataTo(uiA);
+        }
+
+        (uiA as UIA).CheckBinding();
+	}
+	
+	void Update () {
+		
+	}
+}
